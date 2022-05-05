4a1e45e641976f6bc52c45a4c453f1045490b23a
kataldo <kataldo@MacBook-Air-de-kataldo.local>
2022-05-04 20:34:30 -0400

    build 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy_ghp_github_search/",
+  base: "/github_search/",
   plugins: [react()]
 })
