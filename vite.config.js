8b90c8436c6b861794fab00094ba917025a5d1ba
kataldo <kataldo@MacBook-Air-de-kataldo.local>
2022-05-04 20:23:16 -0400

    generate build 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://franciscokataldocl.github.io/github_search/",
+  base: "https://github-search-deploy.vercel.app/",
   plugins: [react()]
 })
